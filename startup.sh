63cb5ec7271372c8d7cc5e916478473f8d4b1815
vladus2000 <docker@matt.land>
2017-07-01 23:10:32 -0500

    fixing startup

--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
+nginx
 nohup su - evil -c 'screen -d -m -S rtorrent rtorrent' > /dev/null 2>&1 &
 nohup su - evil -c 'screen -d -m -S irssi irssi' > /dev/null 2>&1 &
-nginx
+while true
+do
+	sleep 1h
+done
