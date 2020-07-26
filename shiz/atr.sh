e2bbdada80dde3c7900ecffeb766c69ec5edc63d
vladus2000 <docker@matt.land>
2020-07-26 15:49:06 -0500

    atr experiment take 2

--- shiz/atr.sh
+++ shiz/atr.sh
@@ -2,4 +2,4 @@ cd /home/evil/downloads
 mkdir atr
 cd atr
 ln -s /config/atr/config.txt config.txt
-cat /config/atr/config - | python /home/evil/atr/main.py
+{ cat /config/atr/config; tail -f /dev/null; } | python /home/evil/atr/main.py
